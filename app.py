f9db65d4fc351b146ec32684ba1b80d79a394264
Mudrika Sharma <mudrikasharma11@gmail.com>
2026-08-05 23:34:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,13 @@ from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import Chroma
 from langchain_core.prompts import PromptTemplate, ChatPromptTemplate
-from langchain.chains import create_stuff_documents_chain, create_retrieval_chain
+#from langchain.chains import create_stuff_documents_chain, create_retrieval_chain
+
+#--------
+
+# Document Chain Imports (Fixed)
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain.chains.retrieval import create_retrieval_chain
 
 # ... rest of your code follows below ...
 
